282f6fc9c51d17630f0c66d9cfb9fea6b840a76b
Davidson Dorvil <86333511+Dorvil@users.noreply.github.com>
2023-11-19 05:12:28 -0300

    Update portScan.py

--- portScan.py
+++ portScan.py
@@ -1,57 +1,46 @@
+# PortScan - Ferramenta de varredura de portas
+# Desenvolvida por Davidson Dorvil   
+# GitHub: https://github.com/Dorvil
+
 import socket
-import asyncio
-import aiohttp
-from datetime import datetime
 
-def obter_endereco_ip(host):
-    try:
-        endereco_ip = socket.gethostbyname(host)
-        return endereco_ip
-    except socket.gaierror as e:
-        print(f"Erro ao resolver o host: {e}")
-        return None
-
-async def verificar_porta(session, host, porta):
-    url = f"http://{host}:{porta}"
-    try:
-        async with session.get(url, timeout=aiohttp.ClientTimeout(total=5)) as resposta:
-            return porta, resposta.status
-    except aiohttp.ClientError:
-        return porta, None
-    except TimeoutError:
-        return porta, 'timeout'
-
-async def verificar_portas_assincrono(host, portas):
-    endereco_ip = obter_endereco_ip(host)
-
-    print(f"\nPortScanDavid 1.0 scan initiated {datetime.now().strftime('%a %b %d %H:%M:%S %Y')} as: PortScanDavid -p {','.join(map(str, portas))} -sS -oN PortScanDavid.txt {host}")
-    print(f"PortScanDavid report for {host} ({endereco_ip})")
-    print("Host is up (0.000048s latency).\n")
-
-    portas_abertas = []
-    portas_checadas = set()
-    async with aiohttp.ClientSession() as session:
-        tarefas = [verificar_porta(session, host, porta) for porta in portas]
-        for tarefa in asyncio.as_completed(tarefas):
-            porta, resultado = await tarefa
-            print(f"Checando porta {porta}... {'aberta' if resultado == 200 else 'fechada' if resultado == 'timeout' else 'inacessível'}")
-            portas_checadas.add(porta)
-            if resultado == 200:
-                portas_abertas.append((porta, resultado))
-
-    print("\nPortas checadas:")
-    print(', '.join(map(str, sorted(portas_checadas))))
-
-    print("\nPortas abertas:")
-    for porta, resultado in portas_abertas:
-        print(f"PORT     STATE SERVICE\n{porta}/tcp   {'open' if resultado == 200 else 'closed' if resultado == 'timeout' else 'unreachable'}")
-
-    print(f"\nPortScanDavid done at {datetime.now().strftime('%a %b %d %H:%M:%S %Y')} -- 1 IP address (1 host up) scanned in 0.07 seconds by David")
+def realizar_port_scan(host, portas):
+    resultados = {}
+    for porta in portas:
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(1)
+            sock.connect((host, porta))
+            resultados[porta] = "Aberta"
+            sock.close()
+        except (socket.timeout, socket.error):
+            resultados[porta] = "Fechada"
+    
+    return resultados
 
 def main():
-    alvo = input("Digite o endereço IP ou nome do host do alvo: ")
-    portas = range(1, 1025)  # Escaneia as primeiras 1024 portas, você pode ajustar conforme necessário.
-    asyncio.run(verificar_portas_assincrono(alvo, portas))
+    print("******************************************************")
+    print("*                  PortScan - v1.0                    *")
+    print("*               Desenvolvido por Davidson Dorvi       *")
+    print("*                  GitHub: https://github.com/Dorvil  *")
+    print("******************************************************")
+
+    host = input("Digite o endereço IP para realizar a varredura: ")
+    portas_str = input("Digite as portas a serem verificadas (separadas por vírgula): ")
+
+    try:
+        portas = [int(p) for p in portas_str.split(",")]
+    except ValueError:
+        print("Por favor, insira portas válidas.")
+        return
+
+    resultados = realizar_port_scan(host, portas)
+
+    print("\nResultado da Varredura:")
+    print("------------------------")
+    
+    for porta, status in resultados.items():
+        print(f"Porta {porta}: {status}")
 
 if __name__ == "__main__":
     main()
